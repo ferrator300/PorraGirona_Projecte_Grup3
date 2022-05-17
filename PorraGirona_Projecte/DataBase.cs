f3559ae639f8745cea4748c7690acc9c66146f2a
Miquel <a2106911@institutmontilivi.cat>
2022-05-17 12:24:19 +0200

    Canvis a database 1.0

--- PorraGirona_Projecte/DataBase.cs
+++ PorraGirona_Projecte/DataBase.cs
@@ -2,11 +2,114 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
 
 namespace PorraGirona_Projecte
 {
     public class DataBase
     {
+
+        //Miquel
+        string mariaDBConnectionString = "server=localhost;userid=root;database=footballpoll;port3306";
+        MySqlConnection mdbConnection = null;
+
+        public DataBase()
+        {
+            mdbConnection = new MySqlConnection(mariaDBConnectionString);
+        }
+
+        /// <summary>
+        /// Tries to establish a connection with the footballpoll database.
+        /// </summary>
+        /// <returns>true if the connection is successful, or false if it isn't.</returns>
+        public bool Connect()
+        {
+            try
+            {
+                mdbConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Tries to disconnect from the footballpoll database.
+        /// </summary>
+        /// <returns>true if the disconnection is successful, or false if it isn't.</returns>
+        public bool Disconnect()
+        {
+            try
+            {
+                mdbConnection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public int AddPollMember(string name, string surname, string address, string nif, string email)
+        {
+            string command = $"INSERT INTO PollMember(null,'{name}','{surname}','{address}','{nif}','{email}')";
+
+            bool dataIsValid = true;
+
+            //PER SI VOLEM FER COMPROVACIONS A LA CLASSE BASE DE DADES
+
+            //Comprovacions amb Regex o el mètode de validació que acabem fent servir..
+            /* if (false) //nom
+            {
+                dataIsValid = false; 
+            }
+            else if (false) //cognom
+            {
+                dataIsValid = false;
+            }   
+            else if (false) //adreça
+            {
+                dataIsValid = false; 
+            }
+            else if (false) //nif
+            {
+                dataIsValid = false; 
+            }
+            else if (false) //email
+            {
+                dataIsValid = false; 
+            }
+            */
+
+            if (dataIsValid)
+            {
+                try
+                {
+                    MySqlCommand oCommand = new MySqlCommand(command, mdbConnection);
+
+                    oCommand.ExecuteNonQuery();
+
+                    //Retornem true si s'ha pogut fer l'insert correctament.
+                    //D'aquesta manera, el formulari xaml sabrà si el penyista s'ha afegit o no.
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            else
+        }
+
+        //
+
+        //DANIIL
+
+        //
         public ShownMatch ShownMatch
         {
             get => default;
